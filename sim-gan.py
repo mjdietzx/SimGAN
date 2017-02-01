9066c0c9ba9950ddc873cb6a367bdf8b7bdc6e52
Michael <mjdietzx@gmail.com>
2017-02-01 14:47:22 -0600

    bug fix with optional user input pre-trained model paths

--- sim-gan.py
+++ sim-gan.py
@@ -286,4 +286,7 @@ def main(synthesis_eyes_dir, mpii_gaze_dir, refiner_model_path, discriminator_mo
 
 if __name__ == '__main__':
     # TODO: if pre-trained models are passed in, we don't take the steps they've been trained for into account
-    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    refiner_model_path = sys.argv[3] if len(sys.argv) >= 4 else None
+    discriminator_model_path = sys.argv[4] if len(sys.argv) >= 5 else None
+
+    main(sys.argv[1], sys.argv[2], refiner_model_path, discriminator_model_path)
